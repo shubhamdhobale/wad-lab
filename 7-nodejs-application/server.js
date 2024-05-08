a367427056d0b55e2a004aa5f17e34a6fdb4819f
shubhamdhobale2021 <shubhamdhobale2021.it@mmcoe.edu.in>
2024-05-09 01:07:20 +0530

    updated nodejs application

--- 7-nodejs-application/server.js
+++ 7-nodejs-application/server.js
@@ -23,5 +23,5 @@ app.get('/movies' , ( req,res) => {
 
 // Start server
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port http://localhost:${PORT}`);
 });
