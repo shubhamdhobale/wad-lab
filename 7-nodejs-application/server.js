2dc3716e5cc2f3461aa872a8896ab89bf550ccf6
shubhamdhobale2021 <shubhamdhobale2021.it@mmcoe.edu.in>
2024-05-08 10:13:58 +0530

    nodejs application

--- 7-nodejs-application/server.js
+++ 7-nodejs-application/server.js
@@ -1,12 +1,25 @@
 // server.js
 const express = require('express');
-const path = require('path');
-
+// const path = require('path')
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT =  3000;
 
 // Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
+//using middleware
+// app.use(express.static(path.join(__dirname, 'public')));
+
+//defing routes
+app.get('/' , ( req,res) => {
+    res.sendFile('./public/index.html')
+})
+
+app.get('/contact' , ( req,res) => {
+    res.sendFile('./public/contact.html')
+})
+app.get('/movies' , ( req,res) => {
+    res.sendFile('./public/movies.html')
+})
+
 
 // Start server
 app.listen(PORT, () => {
